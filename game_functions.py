162ff3780401ec93c9bbf91265e904efa983634d
greatraksin <greatraksin@icloud.com>
2023-10-06 18:40:23 +0300

    Refactoring check events function

--- game_functions.py
+++ game_functions.py
@@ -2,21 +2,31 @@ import sys
 import pygame as pg
 
 
+def check_keydown_events(event, ship):
+    """Отслеживает нажатие клавиш"""
+    if event.key == pg.K_RIGHT:  # если это кнопка вправо
+        ship.moving_right = True  # разрешаем кораблю двигаться вправо
+    if event.key == pg.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Реагирует на отпускание клавиш"""
+    if event.key == pg.K_RIGHT:  # если это кнопка вправо
+        ship.moving_right = False  # запрещаем кораблю двигаться вправо
+    if event.key == pg.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     for event in pg.event.get():  # обработчик событий pygame
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
         elif event.type == pg.KEYDOWN:  # если кто-то нажал на кнопку
-            if event.key == pg.K_RIGHT:  # если это кнопка вправо
-                ship.moving_right = True  # разрешаем кораблю двигаться вправо
-            elif event.key == pg.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pg.KEYUP:  # если кнопку отпустили
-            if event.key == pg.K_RIGHT:  # если это кнопка вправо
-                ship.moving_right = False  # запрещаем кораблю двигаться вправо
-            elif event.key == pg.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(settings, screen, ship):
