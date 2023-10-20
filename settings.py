8a25fed6614ae990caa81bf4e3155633603a16b2
greatraksin <greatraksin@icloud.com>
2023-10-20 19:08:51 +0300

    Adding bg image

--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
+import pygame as pg
+
 class Settings:
     """Класс для настроек игры (всех)"""
     def __init__(self):
         """Инициализация настроек игры"""
         self.screen_width = 1280
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg = pg.image.load('images/bg.jpg')
+
         self.ship_speed_factor = 1.5
 
         # настройки пули
